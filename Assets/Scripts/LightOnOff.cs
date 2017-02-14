fdeeeb21df60fe884da89105d9f859415ed0d112
jbarba93 <jbarba@hawk.iit.edu>
2017-02-14 00:46:34 -0600

    WEEK 6
    Progressed creating new environment, but was unsuccessful scripting an
    on/off trigger for lights on the sequence to guide the user along the
    path.

---
+++ Assets/Scripts/LightOnOff.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightOnOff : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter()
+    {
+        //Light.enabled = true;        
+    }
+
+    void OnTriggerExit()
+    {
+       // Light.enabled = false;
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
--- Assets/Scripts/RED.cs
+++ Assets/Scripts/RED.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class RED : MonoBehaviour
 {
-
-
+    
     Renderer r;
 
     // Use this for initialization
