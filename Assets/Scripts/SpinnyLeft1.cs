35c0206d32bf61761938ad98079a7b622a4dd0bd
jbarba93 <jbarba@hawk.iit.edu>
2017-02-20 23:48:03 -0600

    WEEK 7
    New objects with coding upon tigger.

---
+++ Assets/Scripts/SpinnyLeft1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinnyLeft1 : MonoBehaviour
+{
+
+
+    // Use this for initialization
+
+    void OnTriggerStay()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        Debug.Log("Stay");
+        transform.Rotate(Vector3.up * Time.deltaTime * 40);
+    }
+
+    void OnTriggerExit()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        //transform.Rotate(Vector3.left * Time.deltaTime * 150);
+    }
+
+}
---
+++ Assets/Scripts/SpinnyRight1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinnyRight1 : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+
+    void OnTriggerStay()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        Debug.Log("Stay");
+        transform.Rotate(Vector3.down * Time.deltaTime * 40);
+    }
+
+    void OnTriggerExit()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        //transform.Rotate(Vector3.left * Time.deltaTime * 150);
+    }
+
+}
