fadfe65990c283da8df5e08d0c1a68e9f651dcd6
jbarba93 <jbarba@hawk.iit.edu>
2017-02-21 11:14:33 -0600

    In Class
    here

---
+++ Assets/Scripts/Easy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Easy : MonoBehaviour
+{
+
+    public GameObject theObject;
+    public static EasingFunction.Ease ease = EasingFunction.Ease.EaseInOutQuad;
+    private EasingFunction.Function func = EasingFunction.GetEasingFunction(ease);
+    private float time = 0;
+    private float interval = 0;
+    private float val = 0;
+    public Transform startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        interval = 10;
+        float t = time % interval;
+        float tn = t / interval;
+        val = func(0, 10, tn);
+        Debug.Log(val);
+        theObject.transform.position = new Vector3(val * 2, 0, 0);
+    }
+}
--- Assets/Scripts/LightOnOff.cs
+++ Assets/Scripts/LightOnOff.cs
@@ -2,20 +2,23 @@
 using System.Collections;
 
 public class LightOnOff : MonoBehaviour {
-
+    public Light myLight;
     // Use this for initialization
     void Start()
     {
+        myLight.intensity = 0;
     }
 
     void OnTriggerEnter()
     {
-        //Light.enabled = true;        
+        //Light.enabled = true;     
+        myLight.intensity = 1;   
     }
 
     void OnTriggerExit()
     {
-       // Light.enabled = false;
+        // Light.enabled = false;
+        myLight.intensity = 0;
     }
     // Update is called once per frame
     void Update () {
